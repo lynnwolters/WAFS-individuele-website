fbd2e5bc44819ea1479a3c5604b9f74bbe424b8c
Lynn Wolters <lynn.wolters27@gmail.com>
2024-02-11 15:24:31 +0100

    Added toggle popup function

--- docs/scripts/popup.js
+++ docs/scripts/popup.js
@@ -1,12 +1,10 @@
+const popup = document.querySelector(".popup");
 const showPopup = document.querySelectorAll(".show-popup");
 const closePopup = document.querySelector(".close-popup");
 
-showPopup.forEach(item => {
-    item.addEventListener("click", togglePopup);
-})
-closePopup.addEventListener("click", togglePopup);
+showPopup.forEach(item => item.addEventListener("click", () => togglePopup()));
+closePopup.addEventListener("click", () => togglePopup());
 
-function togglePopup() {
-    let popup = document.querySelector(".popup");
+const togglePopup = () => {
     popup.classList.toggle("toggle-popup");
 };
