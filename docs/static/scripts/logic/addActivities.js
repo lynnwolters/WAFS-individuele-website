d01f466e6e27e614cd34c1d8a05cb28b2f255363
Lynn Wolters <lynn.wolters27@gmail.com>
2024-02-14 14:29:12 +0100

    Added load more button

--- docs/static/scripts/logic/addActivities.js
+++ docs/static/scripts/logic/addActivities.js
@@ -1,38 +1,76 @@
 export class AddActivities {
     constructor(jsonData) {
-        this.activities = document.querySelector(".activities")
+        this.activitiesContainer = document.querySelector(".activities")
+        this.loadMoreButton = document.querySelector(".load-more-button")
         this.jsonData = jsonData
+        this.activitiesPerPage = 4
+        this.activitiesDisplayed = 0
         this.init()
     }
 
     init = () => {
         this.bindEvents()
+        this.displayActivities()
     }
 
     bindEvents = () => {
-        this.addActivities()
-    }    
+        this.loadMoreButton.addEventListener("click", this.loadMoreActivities)
+    }
 
-    addActivities = () => {
+    displayActivities = () => {
         const itinerariesData = this.jsonData.itineraries
+        const remainingActivities = itinerariesData.reduce((acc, itinerary) => {
+            return acc + itinerary.stops.reduce((acc, stop) => {
+                return acc + stop.activities.length
+            }, 0)
+        }, 0) - this.activitiesDisplayed
 
-        itinerariesData.forEach(itinerary => {
-            itinerary.stops.forEach(stop => {
-                stop.activities.forEach(activity => {
-                    const activityItem = document.createElement("li")
-                    const activityButton = document.createElement("button")
-                    const activityTitle = document.createElement("h3")
-                    const activityImage = document.createElement("img")
+        const activitiesToDisplay = Math.min(this.activitiesPerPage, remainingActivities)
 
-                    this.activities.appendChild(activityItem)
-                    activityItem.appendChild(activityButton)
-                    activityButton.appendChild(activityTitle)
-                    activityButton.appendChild(activityImage)
-                    
-                    activityTitle.textContent = activity.activity
-                    activityImage.src = activity.imgUrl
-                })
-            })
-        })
+        for (let i = 0; i < activitiesToDisplay; i++) {
+            const activityIndex = this.activitiesDisplayed + i
+            const activity = this.getActivityByIndex(activityIndex, itinerariesData)
+            
+            const activityItem = document.createElement("li")
+            const activityButton = document.createElement("button")
+            const activityTitle = document.createElement("h3")
+            const activityImage = document.createElement("img")
+
+            activityItem.classList.add("activity")
+            activityButton.classList.add("activity-button")
+
+            this.activitiesContainer.appendChild(activityItem)
+            activityItem.appendChild(activityButton)
+            activityButton.appendChild(activityTitle)
+            activityButton.appendChild(activityImage)
+
+            activityTitle.textContent = activity.activity
+            activityImage.src = activity.imgUrl
+        }
+
+        this.activitiesDisplayed += activitiesToDisplay
+
+        if (this.activitiesDisplayed >= remainingActivities) {
+            this.loadMoreButton.style.display = "none"
+        }
+    }
+
+    getActivityByIndex = (index, itinerariesData) => {
+        let count = 0
+        for (const itinerary of itinerariesData) {
+            for (const stop of itinerary.stops) {
+                for (const activity of stop.activities) {
+                    if (count === index) {
+                        return activity
+                    }
+                    count++
+                }
+            }
+        }
+        return null
+    }
+
+    loadMoreActivities = () => {
+        this.displayActivities()
     }
 }
